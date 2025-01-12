aff1256d34fa30a7d1114b10cd8cd6b601b5ddb8
manprajapatie <manprajapatie@gmail.com>
2025-01-13 00:23:26 +0530

    Complete Home Banner section

--- amazon/src/Screen/HomeScreen/HomeBanner/HomeBanner.jsx
+++ amazon/src/Screen/HomeScreen/HomeBanner/HomeBanner.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './HomeBanner.css'
+import homebanneritemproduct from '../../../HomeProduct.json'
 
 function HomeBanner() {
     return (
@@ -13,56 +14,42 @@ function HomeBanner() {
 
             {/* ---------- Home Banner Item  --------- */}
             <div className="homeBanneritemDiv">
-                <div className="homeBannerItemDivCard">
-                    <div className="homeBannerItemDivCardTitle">
-                         Title
-                    </div>
 
-                    <div className="imgHomeBannerItemDivCard">
+                {
+                    homebanneritemproduct.product.map((item, ind) => {
+                        return (
+                            <div className="homeBannerItemDivCard">
+                                <div className="homeBannerItemDivCardTitle">
+                                    {item.itemTitle}
+                                </div>
 
-                        <div className="imgBannerHomeDiv" >
-                            <img className = "imgBannerHomeDivImg"
-                                 src="https://images-na.ssl-images-amazon.com/images/G/01/DiscoTec/2024/HomeLifestyle/HomeSummerFlip/Homepage/QuadCards/Home_Flip_Summer_2024_315_HP_NewArrivals_QuadCard_D_01_1x._SY116_CB555960040_.jpg"/>
-                            <div className="imgBannerImgName">
-                                    Ckitchen item
+                                <div className="imgHomeBannerItemDivCard">
+                                    {
+                                        item.imgs.map((it, ind) => {
+                                            return (
+                                                <div className="imgBannerHomeDiv" >
+                                                    <img className="imgBannerHomeDivImg"
+                                                        src={it}/>
+                                                    <div className="imgBannerImgName"> kitchen item </div>
+                                                </div> 
+                                            )
+                                        })
+                                    }
+
+
+                                </div>
+
+                                <div className="seeMoreHomeBanner">
+                                    See More
+                                </div>
+
                             </div>
-                        </div>
-
-                        <div className="imgBannerHomeDiv" >
-                            <img className = "imgBannerHomeDivImg"
-                                 src="https://images-na.ssl-images-amazon.com/images/G/01/DiscoTec/2024/HomeLifestyle/HomeSummerFlip/Homepage/QuadCards/Home_Flip_Summer_2024_316_HP_NewArrivals_QuadCard_D_02_1x._SY116_CB555960040_.jpg"/>
-                            <div className="imgBannerImgName">
-                                    Ckitchen item
-                            </div>
-                        </div>
-
-                        <div className="imgBannerHomeDiv" >
-                            <img className = "imgBannerHomeDivImg"
-                                 src="https://images-na.ssl-images-amazon.com/images/G/01/DiscoTec/2024/HomeLifestyle/HomeSummerFlip/Homepage/QuadCards/Home_Flip_Summer_2024_317_HP_NewArrivals_QuadCard_D_03_1x._SY116_CB555960040_.jpg"/>
-                            <div className="imgBannerImgName">
-                                    Ckitchen ite
-                            </div>
-                        </div>
-
-                        <div className="imgBannerHomeDiv" >
-                            <img className = "imgBannerHomeDivImg"
-                                 src="https://images-na.ssl-images-amazon.com/images/G/01/DiscoTec/2024/HomeLifestyle/HomeSummerFlip/Homepage/QuadCards/Home_Flip_Summer_2024_317_HP_NewArrivals_QuadCard_D_03_1x._SY116_CB555960040_.jpg"/>
-                            <div className="imgBannerImgName">
-                                    Ckitchen ite
-                            </div>
-                        </div>
-
-                      
+                        )
+                    }
+                    )
+                }
 
 
-
-                    </div>
-
-                    <div className="seeMoreHomeBanner">
-                        See More 
-                    </div>
-
-                </div>
             </div>
 
 
