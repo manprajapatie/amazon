1475e093c678b23bc5060a41ec10f17fe3f02c8a
manprajapatie <manprajapatie@gmail.com>
2025-01-15 07:24:40 +0530

    complete Home details section working on Footer

--- amazon/src/Screen/HomeScreen/HomeDetails/HomeDetails.jsx
+++ amazon/src/Screen/HomeScreen/HomeDetails/HomeDetails.jsx
@@ -1,9 +1,78 @@
 import React from 'react'
+import './HomeDetails.css'
 
 const HomeDetails = () => {
   return (
-    <div className=''>
-      Details
+    <div className='HomeDetails'>
+
+      {/* ------------ first Long Card ------------*/}
+
+      <div className='homeDetailsLongCard'>
+        <div className='homeDetailLongCardTitle'>Today's Deals</div>
+        <div className="homeDetailLongCardItems">
+
+          <div className="scrollDiv">
+            
+            <div className='homeDetailLongCardItem'>
+              <img className="homeDetailLongCardItemimg" src="https://m.media-amazon.com/images/I/51IFiSD+kCL._AC_SY200_.jpg" alt="" />
+              <div className='homeDetailLongCardItemImgDetail'>
+
+                <div className='homeDetailLongCardItemTopDetail'>
+                  <div className='homeDetailpercentageoff'>
+                    Up to 20% off
+                  </div>
+
+                  <div className='limitedTimeDealhomeDetail'>
+                    Limited Time Deal
+                  </div>
+                </div>
+
+                <div className="bottomHomeDetail">
+                  iQ00 Z9 | Starting @17999 includ...
+                </div>
+
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
+
+    {/*------------ Second Long Card ------------*/}
+      <div className='homeDetailsLongCard'>
+        <div className='homeDetailLongCardTitle'>Today's Offer</div>
+        <div className="homeDetailLongCardItems">
+
+          <div className="scrollDiv">
+            
+            <div className='homeDetailLongCardItem'>
+              <img className="homeDetailLongCardItemimg" src="https://m.media-amazon.com/images/I/51IFiSD+kCL._AC_SY200_.jpg" alt="" />
+              <div className='homeDetailLongCardItemImgDetail'>
+
+                <div className='homeDetailLongCardItemTopDetail'>
+                  <div className='homeDetailpercentageoff'>
+                    Up to 20% off
+                  </div>
+
+                  <div className='limitedTimeDealhomeDetail'>
+                    Limited Time Deal
+                  </div>
+                </div>
+
+                <div className="bottomHomeDetail">
+                  iQ00 Z9 | Starting @17999 includ...
+                </div>
+
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+
     </div>
   )
 }
