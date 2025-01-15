6ed970422b5c364cda1990f7a936cadac377fcc7
manprajapatie <manprajapatie@gmail.com>
2025-01-15 08:00:13 +0530

    added Products

--- amazon/src/App.jsx
+++ amazon/src/App.jsx
@@ -3,17 +3,22 @@ import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import HomeScreen from './Screen/HomeScreen/HomeScreen'
 import Footer from './Components/Footer/Footer'
+import { Routes, Route } from 'react-router-dom'
+import Products from './Screen/Products/Products'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <div className='App'>
-        <Navbar/>
-        <HomeScreen/>
-        <Footer/>
-     </div>
+      <div className='App'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<HomeScreen/>} />
+          <Route path='/' element={<Products/>} />
+        </Routes>
+        <Footer />
+      </div>
     </>
   )
 }
